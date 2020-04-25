b252fa2e469b1e58b3465e0df1fa60b22bfdd321
ProjectsEpi <62896281+ProjectsEpi@users.noreply.github.com>
2020-04-25 09:20:37 -0700

    Delete f11.m

--- Behavioral_Change/Epidemic_Mexico_City/f11.m
+++
@@ -1,9 +0,0 @@
-function F1 = f11(t,s,ya,ys,N,beta_a,beta_s,rho_t,Lim,Lsm_temp,h11)
-
-if (43 <= t) && (t <= 51)
-    Lsm = h11*Lsm_temp;
-else
-    Lsm = Lsm_temp;
-end
-
-F1 = -(beta_a*ya + beta_s*ys)*s + rho_t*(Lim/N) + (Lsm/N) - (Lim + Lsm)*(s/N);
