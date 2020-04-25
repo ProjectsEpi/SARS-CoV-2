3b98f7a7f9153d2d0849b6904fd070519ffef4a6
ProjectsEpi <62896281+ProjectsEpi@users.noreply.github.com>
2020-04-25 09:21:53 -0700

    Delete f44.m

--- Behavioral_Change/Epidemic_Mexico_City/f44.m
+++
@@ -1,9 +0,0 @@
-function F4 = f44(t,e,ys,N,eta,rho_i,gamma,Lim,Lsm_temp,h11)
-
-if (43 <= t) && (t <= 51)
-    Lsm = h11*Lsm_temp;
-else
-    Lsm = Lsm_temp;
-end
-
-F4 = (1 - rho_i)*gamma*e  - eta*ys - (Lim + Lsm)*(ys/N);
