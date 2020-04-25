052f2191cb61af9d9472ccca831680a968d39c49
ProjectsEpi <62896281+ProjectsEpi@users.noreply.github.com>
2020-04-25 09:21:36 -0700

    Delete f33.m

--- Behavioral_Change/Epidemic_Mexico_City/f33.m
+++
@@ -1,9 +0,0 @@
-function F3 = f33(t,e,ya,N,gamma,rho_i,q_i,rho_t,eta,Lim,Lsm_temp,h11)
-
-if (43 <= t) && (t <= 51)
-    Lsm = h11*Lsm_temp;
-else
-    Lsm = Lsm_temp;
-end
-
-F3 = rho_i*gamma*e + (1 - q_i)*(1 - rho_t)*(Lim/N) - eta*ya - (Lim + Lsm)*(ya/N);
