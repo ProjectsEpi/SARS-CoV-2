e6f7db8c4fb2cbfa3fd022634252021dee6000ea
ProjectsEpi <62896281+ProjectsEpi@users.noreply.github.com>
2020-04-25 09:42:34 -0700

    Delete f22.m

--- R0_And_Dispersal/Travelers/f22.m
+++
@@ -1,9 +0,0 @@
-function F2 = f22(t,s,e,ya,ys,N,beta_a,beta_s,gamma,rho_t,q_i,Lim,Lsm_temp,h11)
-
-if (43 <= t) && (t <= 51)
-    Lsm = h11*Lsm_temp;
-else
-    Lsm = Lsm_temp;
-end
-
-F2 = (beta_a*ya + beta_s*ys)*s + q_i*(1 - rho_t)*(Lim/N) - gamma*e - (Lim + Lsm)*(e/N);
